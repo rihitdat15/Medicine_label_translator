0f4022fef54e227c9801c36c3406e64cc44878ec
Rihit Datta15 <rihitdatta@gmail.com>
2026-04-12 15:05:40 +0530

    Delete backend directory

--- backend/models/response_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class MedicineResponse(BaseModel):
-    purpose: Optional[str]
-    dosage: Optional[str]
-    side_effects: Optional[str]
-    warnings: Optional[str]
-    raw_output: Optional[str] = None
